ca27ad9d6863aef837da67f6c8624dd415a0a378
Eric Websmith <eric.websmith@gmail.com>
2022-08-04 21:07:08 +0800

    leetcode 76

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
+import { useState } from 'react';
 import { NavLink, Route, Routes } from 'react-router-dom';
-import Leetcode322 from './Leetcode/Leetcode322';
+import Leetcode_322 from './Leetcode/Leetcode_322';
+import Leetcode_76 from './Leetcode/Leetcode_76';
 
 function App() {
+  const [showNavbar, setShowNavbar] = useState(true);
+
+  function hide() {
+    setShowNavbar(false);
+  }
+
+  function show() {
+    setShowNavbar(true);
+  }
+
+  const navbar = showNavbar? (
+    <nav className='navbar'>
+        
+    <ul>
+      <li><button onClick={hide}>&lt;&lt;</button><NavLink to="/leetcode_web/">Home</NavLink></li>
+      <li><NavLink to="/leetcode_web/76" >76. Minimum Window Substring</NavLink></li>
+      <li><NavLink to="/leetcode_web/322" >322. Coin Change</NavLink></li>
+    </ul>
+
+  </nav>
+  ):(
+    <button onClick={show}>&gt;&gt;</button>
+  );
   return (
     <main>
-      <nav className='navbar'>
-        
-        <ul>
-          <li><NavLink to="/leetcode_web/">Home</NavLink></li>
-          <li><NavLink to="/leetcode_web/322" >322 Coin Change</NavLink></li>
-          <li><NavLink to="/leetcode_web/378" >378 kth Smallest Element</NavLink></li>
-        </ul>
-
-      </nav>
+      {navbar}
       <div className="article">
         
         <Routes>
-          <Route path="/leetcode_web/" element={<Leetcode322></Leetcode322>}></Route>
-          <Route path="/leetcode_web/322" element={<Leetcode322></Leetcode322>}></Route>
-          <Route path="/leetcode_web/1" element={<h1>1</h1>}></Route>
+          <Route path="/leetcode_web/" element={<Leetcode_322></Leetcode_322>}></Route>
+          <Route path="/leetcode_web/76" element={<Leetcode_76></Leetcode_76>}></Route>
+          <Route path="/leetcode_web/322" element={<Leetcode_322></Leetcode_322>}></Route>
         </Routes>
       </div>
 
--- src/Leetcode/Leetcode_322.js
+++ src/Leetcode/Leetcode_322.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import './Leetcode322.css'
+import './Leetcode_322.css'
+import logo from '../logo.png';
 
 export default function Leetcode322() {
-  const [step, setStep] = useState(2)
+  const [step, setStep] = useState(1)
 
   const states = [
     {
@@ -133,10 +134,14 @@ export default function Leetcode322() {
 
   return (
     <>
-      <h1>322 Coin Change</h1>
+      <header>
+        <img className='logo' src={logo} alt='logo' />
+        <h1>322 Coin Change</h1>
+      </header>
+
       <h2>&nbsp;&nbsp;&nbsp;&nbsp;Example 1</h2>
       <div className="problem">
-        
+
         <table className='mainTable'>
           <tbody>
             <tr className='coinRow' key={'coinRow'}>
@@ -177,72 +182,4 @@ export default function Leetcode322() {
       </div>
     </>
   )
-
-  // return (
-  //   <div className="problem">
-  //     <table>
-  //       <tr>
-  //         <td></td>
-  //         <td><div className='coin'>1</div></td>
-  //         <td><div className='coin'>2</div></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td><div className='coin'>5</div></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //       </tr>
-  //       <tr>
-  //         <td></td>
-  //         <td><i className='fas fa-arrow-down'></i>1</td>
-  //         <td><i className='fas fa-arrow-down'></i>1</td>
-  //         <td></td>
-  //         <td></td>
-  //         <td><i className='fas fa-arrow-down'></i>1</td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //       </tr>
-  //       <tr className='array'>
-  //         <td className='current'>0</td>
-  //         <td><span className='old'>1</span><span className='new'>1</span></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //         <td></td>
-  //       </tr>
-  //       <tr>
-  //         <td>0</td>
-  //         <td>1</td>
-  //         <td>2</td>
-  //         <td>3</td>
-  //         <td>4</td>
-  //         <td>5</td>
-  //         <td>6</td>
-  //         <td>7</td>
-  //         <td>8</td>
-  //         <td>9</td>
-  //         <td>10</td>
-  //         <td>11</td>
-  //       </tr>
-  //     </table>
-  //     <div className='btnbar'>
-  //       <button className='btn'>Previous Step</button>
-  //       <button className='btn'>Next Step</button>
-  //     </div>
-
-  //   </div>
-  // );
 }
