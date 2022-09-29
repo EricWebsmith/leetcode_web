d98972d437e59c3cb00e945cb01de713180f87d7
Eric Websmith <eric.websmith@gmail.com>
2022-09-29 11:17:14 +0800

    658. Find K Closest Elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from 'react';
-import React from 'react'
-import { NavLink, Route, Routes, useParams, useSearchParams } from 'react-router-dom';
+import React from 'react';
+import { NavLink, useSearchParams } from 'react-router-dom';
 
-import Leetcode3 from './Leetcode/Leetcode3';
 import Leetcode19 from './Leetcode/Leetcode19';
-import Leetcode76 from './Leetcode/Leetcode76';
+import Leetcode2421 from './Leetcode/Leetcode2421';
+import Leetcode3 from './Leetcode/Leetcode3';
 import Leetcode322 from './Leetcode/Leetcode322';
 import Leetcode377 from './Leetcode/Leetcode377';
+import Leetcode658 from './Leetcode/Leetcode658';
 import Leetcode659 from './Leetcode/Leetcode659';
+import Leetcode76 from './Leetcode/Leetcode76';
 import Leetcode862 from './Leetcode/Leetcode862';
-import Leetcode2421 from './Leetcode/Leetcode2421';
 
 
 function App(): JSX.Element {
-  const [showNavbar, setShowNavbar] = useState<boolean>(true);
+  const [showNavbar, setShowNavbar] = React.useState<boolean>(true);
 
   function hide() {
     setShowNavbar(false);
@@ -33,6 +33,7 @@ function App(): JSX.Element {
         <li><NavLink to="/leetcode_web/?id=76" >76. Minimum Window Substring</NavLink></li>
         <li><NavLink to="/leetcode_web/?id=322" >322. Coin Change</NavLink></li>
         <li><NavLink to="/leetcode_web/?id=377" >377. Combination Sum IV</NavLink></li>
+        <li><NavLink to="/leetcode_web/?id=658" >658. Find K Closest Elements</NavLink></li>
         <li><NavLink to="/leetcode_web/?id=659" >659. Split Array into Consecutive Subsequences</NavLink></li>
         <li><NavLink to="/leetcode_web/?id=862" >862. Shortest Subarray with Sum at Least K</NavLink></li>
         <li><NavLink to="/leetcode_web/?id=2421" >2421. Number of Good Paths</NavLink></li>
@@ -59,14 +60,16 @@ function LeetcodeSelector() {
   switch (definedId) {
     case 3:
       return (<Leetcode3 />);
-      case 19:
-        return (<Leetcode19 />);
+    case 19:
+      return (<Leetcode19 />);
     case 76:
       return (<Leetcode76 />);
     case 322:
       return (<Leetcode322 />);
     case 377:
       return (<Leetcode377 />);
+    case 658:
+      return (<Leetcode658 />);
     case 659:
       return (<Leetcode659 />);
     case 862:
