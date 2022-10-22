8b2edd7b079b5a6b5c29238bd1a1bf45cecf87b8
Eric Websmith <eric.websmith@gmail.com>
2022-10-22 12:49:37 +0800

    76. Minimum Window Substring

--- src/Leetcode/Leetcode0076.tsx
+++ src/Leetcode/Leetcode0076.tsx
@@ -1,13 +1,13 @@
 import React, { SetStateAction } from 'react';
 import ButtonBar from '../controls/ButtonBar';
 import SvgTexts from '../controls/SvgTexts';
-import { getChildren, getChildrenFromRef, getElementById } from '../Utils/html';
+import { getChildrenFromRef, getElementById } from '../Utils/html';
 
 type Frame = {
   left: number;
   right: number;
   ans: string;
-  movingBag: number[];
+  need: string;
   isSubset?: boolean;
 };
 
@@ -15,32 +15,32 @@ export default function Leetcode0076() {
   const [frameIndex, setFrameIndex] = React.useState<number>(0);
 
   const frames: Frame[] = [
-    { left: 1000, right: -1, movingBag: [0, 0, 0], ans: '""' },
-    { left: 0, right: 0, movingBag: [1, 0, 0], ans: '""' },
-    { left: 0, right: 1, movingBag: [1, 0, 0], ans: '""' },
-    { left: 0, right: 2, movingBag: [1, 0, 0], ans: '""' },
-    { left: 0, right: 3, movingBag: [1, 1, 0], ans: '""' },
-    { left: 0, right: 4, movingBag: [1, 1, 0], ans: '""' },
-    { left: 0, right: 5, movingBag: [1, 1, 1], isSubset: true, ans: 'ADOBEC' },
-    { left: 1, right: 5, movingBag: [0, 1, 1], ans: 'ADOBEC' },
-    { left: 1, right: 6, movingBag: [0, 1, 1], ans: 'ADOBEC' },
-    { left: 1, right: 7, movingBag: [0, 1, 1], ans: 'ADOBEC' },
-    { left: 1, right: 8, movingBag: [0, 1, 1], ans: 'ADOBEC' },
-    { left: 1, right: 9, movingBag: [0, 2, 1], ans: 'ADOBEC' },
-    { left: 1, right: 10, movingBag: [1, 2, 1], isSubset: true, ans: 'ADOBEC' },
-    { left: 2, right: 10, movingBag: [1, 2, 1], isSubset: true, ans: 'ADOBEC' },
-    { left: 3, right: 10, movingBag: [1, 2, 1], isSubset: true, ans: 'ADOBEC' },
-    { left: 4, right: 10, movingBag: [1, 1, 1], isSubset: true, ans: 'ADOBEC' },
-    { left: 5, right: 10, movingBag: [1, 1, 1], isSubset: true, ans: 'ADOBEC' },
-    { left: 6, right: 10, movingBag: [1, 1, 0], ans: 'ADOBEC' },
-    { left: 6, right: 11, movingBag: [1, 1, 0], ans: 'ADOBEC' },
-    { left: 6, right: 12, movingBag: [1, 1, 1], isSubset: true, ans: 'ADOBEC' },
-    { left: 7, right: 12, movingBag: [1, 1, 1], isSubset: true, ans: 'ADOBEC' },
-    { left: 8, right: 12, movingBag: [1, 1, 1], isSubset: true, ans: 'EBANC' },
-    { left: 9, right: 12, movingBag: [1, 1, 1], isSubset: true, ans: 'BANC' },
-    { left: 10, right: 12, movingBag: [1, 0, 1], ans: 'BANC' },
-    { left: 11, right: 12, movingBag: [0, 0, 1], ans: 'BANC' },
-    { left: 12, right: 12, movingBag: [0, 0, 1], ans: 'BANC' },
+    { left: 1000, right: -1, need: ' 1 1 1', ans: '""' },
+    { left: 0, right: 0, need: ' 0 1 1', ans: '""' },
+    { left: 0, right: 1, need: ' 0 1 1', ans: '""' },
+    { left: 0, right: 2, need: ' 0 1 1', ans: '""' },
+    { left: 0, right: 3, need: ' 0 0 1', ans: '""' },
+    { left: 0, right: 4, need: ' 0 0 1', ans: '""' },
+    { left: 0, right: 5, need: ' 0 0 0', isSubset: true, ans: 'ADOBEC' },
+    { left: 1, right: 5, need: ' 1 0 0', ans: 'ADOBEC' },
+    { left: 1, right: 6, need: ' 1 0 0', ans: 'ADOBEC' },
+    { left: 1, right: 7, need: ' 1 0 0', ans: 'ADOBEC' },
+    { left: 1, right: 8, need: ' 1 0 0', ans: 'ADOBEC' },
+    { left: 1, right: 9, need: ' 1-1 0', ans: 'ADOBEC' },
+    { left: 1, right: 10, need: ' 0-1 0', isSubset: true, ans: 'ADOBEC' },
+    { left: 2, right: 10, need: ' 0-1 0', isSubset: true, ans: 'ADOBEC' },
+    { left: 3, right: 10, need: ' 0-1 0', isSubset: true, ans: 'ADOBEC' },
+    { left: 4, right: 10, need: ' 0 0 0', isSubset: true, ans: 'ADOBEC' },
+    { left: 5, right: 10, need: ' 0 0 0', isSubset: true, ans: 'ADOBEC' },
+    { left: 6, right: 10, need: ' 0 0 1', ans: 'ADOBEC' },
+    { left: 6, right: 11, need: ' 0 0 1', ans: 'ADOBEC' },
+    { left: 6, right: 12, need: ' 0 0 0', isSubset: true, ans: 'ADOBEC' },
+    { left: 7, right: 12, need: ' 0 0 0', isSubset: true, ans: 'ADOBEC' },
+    { left: 8, right: 12, need: ' 0 0 0', isSubset: true, ans: 'EBANC' },
+    { left: 9, right: 12, need: ' 0 0 0', isSubset: true, ans: 'BANC' },
+    { left: 10, right: 12, need: ' 0 1 0', ans: 'BANC' },
+    { left: 11, right: 12, need: ' 1 0 0', ans: 'BANC' },
+    { left: 12, right: 12, need: ' 0 0 0', ans: 'BANC' },
   ];
 
   const step = 65;
@@ -80,41 +80,9 @@ export default function Leetcode0076() {
       rect.setAttribute('fill', 'blue');
     }
 
-    if (frame.isSubset == true) {
+    if (frame.isSubset) {
       for (let i = frame.left; i <= frame.right; i++) {
         rects[i].setAttribute('fill', 'green');
-      }
-    }
-
-    //superset sign
-    const supersetControl = getElementById('superset');
-    if (frame.isSubset == true) {
-      supersetControl.innerHTML = '⊇';
-      supersetControl.setAttribute('fill', 'green');
-    } else {
-      supersetControl.innerHTML = '⊅';
-      supersetControl.setAttribute('fill', 'red');
-    }
-
-    // moving bag numbers
-    const movingBagControl = getElementById('moving-bag-numbers');
-    const movingBagNumbers = getChildren<SVGRectElement>(movingBagControl);
-    for (let i = 0; i < 3; i++) {
-      movingBagNumbers[i].textContent = `${frame.movingBag[i]}`;
-    }
-
-    // moving-bag-rects
-    const movingBagRectControl = getElementById('moving-bag-rects');
-    const movingBagRects = getChildren<SVGRectElement>(movingBagRectControl);
-    for (let i = 0; i < 3; i++) {
-      movingBagRects[i].setAttribute('fill', 'blue');
-    }
-    if (previousFrame != null) {
-      for (let i = 0; i < 3; i++) {
-        movingBagRects[i].setAttribute('fill', 'blue');
-        if (frame.movingBag[i] != previousFrame.movingBag[i]) {
-          movingBagRects[i].setAttribute('fill', 'red');
-        }
       }
     }
   }, [frameIndex]);
@@ -148,7 +116,7 @@ export default function Leetcode0076() {
       <svg
         id='svg'
         width={950}
-        height={550}
+        height={400}
         style={{
           backgroundColor: 'white',
           cursor: 'url(Laser_Pointer.png), pointer!important',
@@ -242,9 +210,9 @@ export default function Leetcode0076() {
             12
           </text>
         </g>
-        <g id='tbag'>
-          <text x='60' y='260' fill='black' style={titleStyle}>
-            Moving-BAG
+        <g>
+          <text x='120' y='260' fill='black' style={titleStyle}>
+            NEED
           </text>
           <g
             id='tbagIndex'
@@ -272,43 +240,22 @@ export default function Leetcode0076() {
             <rect x={200} y={310} height='70' width='70'></rect>
             <rect x={280} y={310} height='70' width='70'></rect>
           </g>
-          <g
-            id='moving-bag-numbers'
-            style={{
-              fontSize: '60px',
-              fill: 'white',
-              fontFamily: 'Arial Black',
-            }}>
-            <text x='55' y='365'>
-              0
-            </text>
-            <text x='135' y='365'>
-              0
-            </text>
-            <text x='215' y='365'>
-              0
-            </text>
-            <text x='285' y='365'>
-              ...
-            </text>
-          </g>
+          <SvgTexts
+            x={55}
+            y={365}
+            offsetX={80}
+            text={frame.need}
+            step={2}
+            spaceOffset={0}
+            style={{ fontSize: '60px', fill: 'white', fontFamily: 'Arial Black' }}></SvgTexts>
+          <text x='285' y='365' style={{ fontSize: '60px', fill: 'white', fontFamily: 'Arial Black' }}>
+            ...
+          </text>
         </g>
-        <text x='400' y='260' fill='black' style={titleStyle}>
-          Has?
-        </text>
-        <text
-          id='superset'
-          x='400'
-          y='380'
-          fill='black'
-          style={{
-            fontSize: 120,
-          }}>
-          ⊇⊅
-        </text>
-        <g id='moving-bag' transform='translate(510,0)'>
+
+        <g id='moving-bag' transform='translate(410,0)'>
           <text x='60' y='260' fill='black' style={titleStyle}>
-            Target-BAG
+            Current Answer
           </text>
           <g
             id='tbagIndex'
@@ -316,66 +263,18 @@ export default function Leetcode0076() {
               fontSize: '32px',
               fontFamily: 'Arial Black',
             }}>
-            <text x='65' y='300'>
-              A
-            </text>
-            <text x='145' y='300'>
-              B
-            </text>
-            <text x='225' y='300'>
-              C
-            </text>
-          </g>
-          <g
-            id='tbagRects'
-            style={{
-              fill: 'blue',
-            }}>
-            <rect x={40} y={310} height='70' width='70'></rect>
-            <rect x={120} y={310} height='70' width='70'></rect>
-            <rect x={200} y={310} height='70' width='70'></rect>
-            <rect x={280} y={310} height='70' width='70'></rect>
-          </g>
-          <g
-            id='tbag'
-            style={{
-              fontSize: '60px',
-              fill: 'white',
-              fontFamily: 'Arial Black',
-            }}>
-            <text x='55' y='365'>
-              1
-            </text>
-            <text x='135' y='365'>
-              1
-            </text>
-            <text x='215' y='365'>
-              1
-            </text>
-            <text x='285' y='365'>
-              ...
+            <text
+              id='ans'
+              x='65'
+              y='350'
+              style={{
+                fontSize: '48px',
+                fontFamily: 'Arial Black',
+              }}>
+              {frame.ans}
             </text>
           </g>
         </g>
-        <text
-          x='50'
-          y='470'
-          style={{
-            fontSize: '32px',
-            fontFamily: 'Arial Black',
-          }}>
-          Current Answer:
-        </text>
-        <text
-          id='ans'
-          x='360'
-          y='470'
-          style={{
-            fontSize: '48px',
-            fontFamily: 'Arial Black',
-          }}>
-          {frame.ans}
-        </text>
       </svg>
       <ButtonBar setIndex={setIndex} />
     </>
